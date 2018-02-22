ac0d09d9758ab1a6ae4a7ef132f3e2e91d7b0aa6
Zsolt Makrai <zsolt.makrai@blackswan.com>
2018-02-22 17:57:46 +0100

    feat(app): add eventListener to roll dice and display the dice

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -12,8 +12,22 @@ GAME RULES:
 let scores = [0, 0]
 let roundScore = 0
 let activePlayer = 0
-let dice = Math.floor(Math.random() * 6) + 1
-
-document.querySelector('#current-' + activePlayer).textContent = dice
 
 document.querySelector('.dice').style.display = 'none'
+document.getElementById('score-0').textContent = 0
+document.getElementById('score-1').textContent = 0
+document.getElementById('current-0').textContent = 0
+document.getElementById('current-1').textContent = 0
+
+document.querySelector('.btn-roll').addEventListener ('click', function () {
+	let dice = Math.floor(Math.random() * 6) + 1
+	let diceDom = document.querySelector('.dice')
+
+	diceDom.style.display = 'block'
+	diceDom.src = 'dice-' + dice + '.png'
+})
+
+
+
+
+// document.querySelector('#current-' + activePlayer).textContent = dice
