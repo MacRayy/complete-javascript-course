94670c3663df9be2067b24f2c71dac66d577b77f
Zsolt Makrai <zsolt.makrai@blackswan.com>
2018-02-27 11:03:26 +0100

    feat(app): add init function

--- Budgety/app.js
+++ Budgety/app.js
@@ -4,20 +4,32 @@ import UIController from './view.js'
 import budgetController from './logic.js'
 
 const controller = function (budgetCtrl, UICtrl) {
-	const DOM = UICtrl.DOMStrings
+	const setupEventListeners = () => {
+		const DOM = UICtrl.DOMStrings
+
+		document.querySelector(DOM.addBtn).addEventListener('click', ctrlAddItem)
+
+		document.addEventListener('keypress', (event) => {
+			if (event.keyCode === 13 || event.which === 13) {
+				ctrlAddItem()
+			}
+		})
+	}
 
 	const ctrlAddItem = () => {
 		const input = UICtrl.getInput()
 		console.log(input)
 	}
 
-	document.querySelector(DOM.addBtn).addEventListener('click', ctrlAddItem)
-
-	document.addEventListener('keypress', (event) => {
-		if (event.keyCode === 13 || event.which === 13) {
-			ctrlAddItem()
+	return {
+		init: () => {
+			console.log('start')
+			setupEventListeners()
 		}
-	})
+	}
 }
 
 controller(budgetController, UIController)
+
+// START APP
+controller.init()
