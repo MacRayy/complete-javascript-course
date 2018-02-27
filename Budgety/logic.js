bef3a556baa4f400c8c88cd338df55401671eb71
Zsolt Makrai <zsolt.makrai@blackswan.com>
2018-02-27 11:33:27 +0100

    feat(logic): add basic data structure and function constructors

--- Budgety/logic.js
+++ Budgety/logic.js
@@ -1,7 +1,28 @@
 'use strict'
 
 const budgetController = (function () {
+	const budgetData = {
+		allItems: {
+			exp: [],
+			inc: []
+		},
+		totals: {
+			exp: 0,
+			inc: 0
+		}
+	}
 
+	const Expense = (id, description, value) => {
+		this.id = id
+		this.description = description
+		this.value = value
+	}
+
+	const Income = (id, description, value) => {
+		this.id = id
+		this.description = description
+		this.value = value
+	}
 })()
 
 export default budgetController
