8cc3fa8e856039aef6bb8fd3f63c25f41cc8b356
Zsolt Makrai <zsolt.makrai@blackswan.com>
2018-02-27 14:20:33 +0100

    feat(logic): save the input into the date structure, refactor the constructor functions into classes

--- Budgety/app.js
+++ Budgety/app.js
@@ -4,10 +4,10 @@ import UIController from './view.js'
 import budgetController from './logic.js'
 
 const controller = function (budgetCtrl, UICtrl) {
-		const init = () => {
-			console.log('start')
-			setupEventListeners()
-		}
+	const init = () => {
+		console.log('start')
+		setupEventListeners()
+	}
 
 	const setupEventListeners = () => {
 		const DOM = UICtrl.DOMStrings
@@ -22,8 +22,19 @@ const controller = function (budgetCtrl, UICtrl) {
 	}
 
 	const ctrlAddItem = () => {
+		// 1. Get the filed input data
 		const input = UICtrl.getInput()
 		console.log(input)
+
+		// 2. Add the item to the budget controller
+		const newItem = budgetCtrl.addItem(input.type, input.description, input.value)
+		console.log(budgetCtrl.budgetData)
+
+		// 3. Add the item to the UI
+
+		// 4. Calculate the budget
+
+		// 5. Display the budget on the UI
 	}
 
 	return {
--- Budgety/logic.js
+++ Budgety/logic.js
@@ -12,16 +12,63 @@ const budgetController = (function () {
 		}
 	}
 
-	const Expense = (id, description, value) => {
-		this.id = id
-		this.description = description
-		this.value = value
+	// ES5
+	// const Expense = function (id, description, value) {
+	// 	this.id = id
+	// 	this.description = description
+	// 	this.value = value
+	// }
+
+	// const Income = function (id, description, value) {
+	// 	this.id = id
+	// 	this.description = description
+	// 	this.value = value
+	// }
+
+	// ES6
+	class Expense {
+		constructor (id, description, value) {
+			this.id = id
+			this.description = description
+			this.value = value
+		}
 	}
 
-	const Income = (id, description, value) => {
-		this.id = id
-		this.description = description
-		this.value = value
+	class Income {
+		constructor (id, description, value) {
+			this.id = id
+			this.description = description
+			this.value = value
+		}
+	}
+
+	const addItem = (type, des, val) => {
+		let newItem
+		let ID = 0
+
+		// Create new ID
+		if (budgetData.allItems[type].length > 0) {
+			// ID = budgetData.allItes[type][budgetData.allItems[type].length - 1].id + 1
+			ID = [...budgetData.allItems[type]].pop().id + 1
+		}
+
+		//Create new item based on 'inc' or 'exp' type
+		if (type === 'exp') {
+			newItem = new Expense(ID, des, val)
+		} else if (type === 'inc') {
+			newItem = new Income(ID, des, val)
+		}
+
+		//Push it into date structure
+		budgetData.allItems[type].push(newItem)
+
+		//Return new item
+		return newItem
+	}
+
+	return {
+		addItem,
+		budgetData
 	}
 })()
 
