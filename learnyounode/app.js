d78d40691430b61b0efdbac54e50fab3c3ebc8b5
Zsolt Makrai <zsolt.makrai@blackswan.com>
2018-03-28 19:44:48 +0200

    feat(node): count the lines in the file

---
+++ learnyounode/app.js
@@ -0,0 +1,18 @@
+'use strict'
+
+// const sum = () => {
+// 	const args = process.argv.slice(2)
+// 	let sum = 0
+// 	for (let i = 0; i < args.length; i++) {
+// 		sum += parseInt(args[i])
+// 	}
+// 	return console.log(sum)
+// }
+
+// sum()
+
+const fs = require('fs')
+
+const countLines = () => console.log(fs.readFileSync(process.argv[2], {encoding: 'utf8'}).split('\n').length - 1)
+
+countLines()
